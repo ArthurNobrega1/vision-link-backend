7f16d9bebf74dc27f79417be3f416f2a1872aabf
ArthurNobrega1 <arthurnobrega906@gmail.com>
2024-11-15 14:35:42 -0300

    feat(user): Remove user now remove routes of user too

--- src/modules/routes/repositories/IRoutesRepository.ts
+++ src/modules/routes/repositories/IRoutesRepository.ts
@@ -1,8 +1,10 @@
+import { Types } from "mongoose"
 import IRouteDTO from "../dto/IRouteDTO"
 
 interface IRoutesRepository {
     find(data: {}): Promise<IRouteDTO[]>
     create(data: IRouteDTO): Promise<IRouteDTO>
+    deleteMany(filter: { userId: string | Types.ObjectId }): Promise<{ deletedCount: number }>
 }
 
 export default IRoutesRepository
